3a97df7f63018950482ff3e7fa7b1c2b65ed8c48
huaihong1986 <huaihong1986@sina.com>
2015-08-21 08:46:25 +0800

    Initial Commit

---
+++ MyPlist/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyPlist
+//
+//  Created by Hu Aihong on 15-8-21.
+//  Copyright (c) 2015年 ChinaCloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyPlist/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyPlist
+//
+//  Created by Hu Aihong on 15-8-21.
+//  Copyright (c) 2015年 ChinaCloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
